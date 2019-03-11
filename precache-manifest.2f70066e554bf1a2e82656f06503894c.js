87e6ebaeacb237f42652c9d7ca75ee22e39165d0
Lexi <ambellinger24@gmail.com>
2019-03-10 21:47:37 -0500

    Updates

---
+++ precache-manifest.2f70066e554bf1a2e82656f06503894c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f8cf428aeba88790c600",
+    "url": "/clicky_game/static/js/runtime~main.f8cf428a.js"
+  },
+  {
+    "revision": "da7f83ef58b3b521b211",
+    "url": "/clicky_game/static/js/main.da7f83ef.chunk.js"
+  },
+  {
+    "revision": "ff661de6195320cfc0eb",
+    "url": "/clicky_game/static/js/2.ff661de6.chunk.js"
+  },
+  {
+    "revision": "da7f83ef58b3b521b211",
+    "url": "/clicky_game/static/css/main.29fa02cb.chunk.css"
+  },
+  {
+    "revision": "fdc7dc8de6b5dca343ede93a3e581c0a",
+    "url": "/clicky_game/index.html"
+  }
+];
